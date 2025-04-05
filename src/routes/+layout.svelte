c9af85d4031576a3cb345b902ab07fedd7622b83
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-05 16:15:06 +0200

    feat: Adapted template-frontend to the chat app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '$styles/app.css';
+	import { type Snippet } from 'svelte';
+
+	import pageTitle from '$lib/stores/ui/pageTitle';
+	import heroContainer from '$lib/stores/ui/heroContainer';
+
+	import { HeroContainer } from '@totocorpsoftwareinc/frontend-toolkit';
+
+	interface Props {
+		children?: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>{$pageTitle}</title>
+</svelte:head>
+<HeroContainer
+	width={$heroContainer.width}
+	height={$heroContainer.height}
+	bgColor={$heroContainer.color}
+	bgOverlay="bg-transparent"
+>
+	{@render children?.()}
+</HeroContainer>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+	plugins: [sveltekit(), tailwindcss()],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			reporter: ['json'],
+			include: ['src/**/*.{js,ts,svelte}'],
+			exclude: ['**/index.ts']
+		}
+	}
+});

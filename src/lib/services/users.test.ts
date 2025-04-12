8b4ed5850dc65a4c25332e9f9c458631e20f9ebc
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-04-12 11:17:12 +0200

    feat: When a user exists, we fetch its details and proceed to login

--- src/lib/services/users.test.ts
+++ src/lib/services/users.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, assert, expect, afterEach } from 'vitest';
 import fetchMock from 'fetch-mock';
-import { createChatUser } from '$lib/services/users';
+import { createChatUser, getChatUser, listUsersByName } from '$lib/services/users';
 import { createRouteMatcher } from '$lib/services/testUtils';
 
 describe('Create chat user', () => {
@@ -71,5 +71,117 @@ describe('Create chat user', () => {
 			api_user: '00000000-0000-0000-0000-000000000000',
 			created_at: '2025-04-08T18:15:52.639255Z'
 		});
+	});
+});
+
+describe('Get chat user', () => {
+	afterEach(async () => {
+		fetchMock.unmockGlobal();
+		fetchMock.hardReset();
+	});
+
+	it('should use route from env and consistent with provided id', async () => {
+		const expectedUrl =
+			'http://localhost:60001/v1/chats/users/5158aaa7-e540-432b-8cce-da9ccfe71d80';
+		fetchMock.mockGlobal().route(createRouteMatcher(expectedUrl, 'get'), 200, 'getChatUserRoute');
+
+		await getChatUser('5158aaa7-e540-432b-8cce-da9ccfe71d80');
+
+		assert(fetchMock.callHistory.called('getChatUserRoute'));
+	});
+
+	it('should return data received from server', async () => {
+		const mockedResponse = {
+			requestId: '2fe6fb33-08a3-426b-ad51-ef4f8bf208fa',
+			status: 'SUCCESS',
+			details: {
+				id: '0198ed26-8e92-4b81-aec0-aaaff33b6a11',
+				name: 'user1',
+				api_user: '0463ed3d-bfc9-4c10-b6ee-c223bbca0fab',
+				created_at: '2025-04-06T18:50:40.525234Z'
+			}
+		};
+
+		const expectedUrl =
+			'http://localhost:60001/v1/chats/users/fe4cae61-7847-4342-a5d7-900576a81324';
+		fetchMock.mockGlobal().get(createRouteMatcher(expectedUrl, 'get'), {
+			status: 200,
+			body: mockedResponse
+		});
+
+		const actual = await getChatUser('fe4cae61-7847-4342-a5d7-900576a81324');
+
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.getDetails()).toEqual({
+			id: '0198ed26-8e92-4b81-aec0-aaaff33b6a11',
+			name: 'user1',
+			api_user: '0463ed3d-bfc9-4c10-b6ee-c223bbca0fab',
+			created_at: '2025-04-06T18:50:40.525234Z'
+		});
+	});
+});
+
+describe('List chat users by name', () => {
+	afterEach(async () => {
+		fetchMock.unmockGlobal();
+		fetchMock.hardReset();
+	});
+
+	it('should use route from env and consistent with provided id', async () => {
+		const expectedUrl = 'http://localhost:60001/v1/chats/users?name=user1';
+		fetchMock
+			.mockGlobal()
+			.route(createRouteMatcher(expectedUrl, 'get'), 200, 'listChatUsersByNameRoute');
+
+		await listUsersByName('user1');
+
+		assert(fetchMock.callHistory.called('listChatUsersByNameRoute'));
+	});
+
+	it('should encode names with special characters', async () => {
+		// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/toString
+		// https://www.url-encode-decode.com/
+		const expectedUrl =
+			'http://localhost:60001/v1/chats/users?name=my+special+%2Fchars%5C+user+%F0%9F%98%8A+%26';
+		fetchMock
+			.mockGlobal()
+			.route(createRouteMatcher(expectedUrl, 'get'), 200, 'listChatUsersByNameRoute');
+
+		await listUsersByName('my special /chars\\ user 😊 &');
+
+		assert(fetchMock.callHistory.called('listChatUsersByNameRoute'));
+	});
+
+	it('should return data received from server', async () => {
+		const mockedResponse = {
+			requestId: '2fe6fb33-08a3-426b-ad51-ef4f8bf208fa',
+			status: 'SUCCESS',
+			details: [
+				{
+					id: '0198ed26-8e92-4b81-aec0-aaaff33b6a11',
+					name: 'user1',
+					api_user: '0463ed3d-bfc9-4c10-b6ee-c223bbca0fab',
+					created_at: '2025-04-06T18:50:40.525234Z'
+				}
+			]
+		};
+
+		const expectedUrl = 'http://localhost:60001/v1/chats/users?name=user1';
+		fetchMock.mockGlobal().get(createRouteMatcher(expectedUrl, 'get'), {
+			status: 200,
+			body: mockedResponse
+		});
+
+		const actual = await listUsersByName('user1');
+
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.getDetails()).toEqual([
+			{
+				id: '0198ed26-8e92-4b81-aec0-aaaff33b6a11',
+				name: 'user1',
+				api_user: '0463ed3d-bfc9-4c10-b6ee-c223bbca0fab',
+				created_at: '2025-04-06T18:50:40.525234Z'
+			}
+		]);
 	});
 });

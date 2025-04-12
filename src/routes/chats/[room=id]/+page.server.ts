8c6652836203735355a43c425977fd7bd66ff959
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-04-12 12:04:28 +0200

    feat: Added copy of chats page which fetches the messages for a specific room

---
+++ src/routes/chats/[room=id]/+page.server.ts
@@ -0,0 +1,62 @@
+import { loadCookiesOrRedirectToLogin, resetChatCookies } from '$lib/cookies';
+import { handleApiError } from '$lib/rest/api';
+import { getChatUser } from '$lib/services/users';
+import { chatUserResponseDtoToChatUserUiDto } from '$lib/converters/chatUserConverter';
+import { error, redirect } from '@sveltejs/kit';
+import {
+	HttpStatus,
+	parseApiResponseAsArray,
+	parseApiResponseAsSingleValue
+} from '@totocorpsoftwareinc/frontend-toolkit';
+import { ChatUserResponseDto } from '$lib/communication/api/chatUserResponseDto';
+import { getRoomsForUser } from '$lib/services/rooms';
+import { RoomResponseDto } from '$lib/communication/api/roomResponseDto';
+import { roomResponseDtoToRoomUiDto } from '$lib/converters/roomConverter.js';
+import { getMessagesForRoom } from '$lib/services/messages.js';
+import { MessageResponseDto } from '$lib/communication/api/messageResponseDto.js';
+import { messageResponseDtoToMessageUiDto } from '$lib/converters/messageConverter.js';
+
+export async function load({ params, cookies }) {
+	const chatCookies = loadCookiesOrRedirectToLogin(cookies);
+
+	// Fetch the user's data
+	let apiResponse = await getChatUser(chatCookies.chatUser);
+	handleApiError(apiResponse);
+
+	const chatUserDto = parseApiResponseAsSingleValue(apiResponse, ChatUserResponseDto);
+	if (chatUserDto === undefined) {
+		error(HttpStatus.INTERNAL_SERVER_ERROR, 'Failed to get user data');
+	}
+
+	// Fetch the rooms the user belongs to
+	apiResponse = await getRoomsForUser(chatUserDto.id);
+	handleApiError(apiResponse);
+
+	const rooms = parseApiResponseAsArray(apiResponse, RoomResponseDto);
+	if (rooms === undefined) {
+		error(HttpStatus.INTERNAL_SERVER_ERROR, 'Failed to get rooms data');
+	}
+
+	// Fetch the messages for the current room
+	apiResponse = await getMessagesForRoom(params.room);
+	handleApiError(apiResponse);
+
+	const messages = parseApiResponseAsArray(apiResponse, MessageResponseDto);
+	if (messages === undefined) {
+		error(HttpStatus.INTERNAL_SERVER_ERROR, 'Failed to get messages data');
+	}
+
+	return {
+		user: chatUserResponseDtoToChatUserUiDto(chatUserDto),
+		rooms: rooms.map((room) => roomResponseDtoToRoomUiDto(room)),
+		messages: messages.map((message) => messageResponseDtoToMessageUiDto(message))
+	};
+}
+
+export const actions = {
+	logout: async ({ cookies }) => {
+		resetChatCookies(cookies);
+
+		redirect(HttpStatus.SEE_OTHER, '/');
+	}
+};

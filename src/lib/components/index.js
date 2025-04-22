1cb1344e617dc411ebfa9f2811170528e776355f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-04-22 21:47:43 +0200

    feat: Added MessageInput component

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,2 +1,3 @@
 export { default as RoomsList } from '$lib/components/RoomsList.svelte';
 export { default as MessagesArea } from '$lib/components/MessagesArea.svelte';
+export { default as MessageInput } from '$lib/components/MessageInput.svelte';

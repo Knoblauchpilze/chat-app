5e36e8a87367aed6445ad2730baa2b86711f823f
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-14 20:13:26 +0200

    chore: Removed unnecessary test setup for websockets

--- tests/mocks/constants.ts
+++
@@ -1,2 +0,0 @@
-export const TEST_SERVER_HOST = 'localhost';
-export const TEST_SERVER_PORT = 9999;
--- tests/utils.ts
+++
@@ -1,4 +0,0 @@
-// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-export async function waitFor(timeoutMs: number) {
-	await new Promise((resolve) => setTimeout(resolve, timeoutMs));
-}

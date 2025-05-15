df0ac0927ce88e6d54e0251b5264ee8b9d465434
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-15 08:05:42 +0200

    feat: Receiving messages adds them to the list and displays them

--- src/lib/converters/messageConverter.ts
+++ src/lib/converters/messageConverter.ts
@@ -1,11 +1,28 @@
 import type { ChatUserResponseDto } from '$lib/communication/api/chatUserResponseDto';
 import type { MessageResponseDto } from '$lib/communication/api/messageResponseDto';
+import type { ChatUserUiDto } from '$lib/communication/ui/chatUserUiDto';
 import type { MessageUiDto } from '$lib/communication/ui/messageUiDto';
 import { formatDate } from '$lib/time';
 
 export function messageResponseDtoToMessageUiDto(
 	apiDto: MessageResponseDto,
 	users: ChatUserResponseDto[]
+): MessageUiDto {
+	const maybeUser = users.find((user) => user.id === apiDto.user);
+
+	return {
+		id: apiDto.id,
+		userId: apiDto.user,
+		user: maybeUser?.name ?? 'Ghost',
+		room: apiDto.room,
+		message: apiDto.message,
+		createdAt: formatDate(apiDto.createdAt)
+	};
+}
+
+export function messageResponseDtoToMessageUiDtoFromUiUser(
+	apiDto: MessageResponseDto,
+	users: ChatUserUiDto[]
 ): MessageUiDto {
 	const maybeUser = users.find((user) => user.id === apiDto.user);
 

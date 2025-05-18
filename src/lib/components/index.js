351280e391b575e17486f1aca83f89cd251dce5a
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-18 16:20:16 +0200

    chore: Extracted svg to Svelte components

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,4 +1,8 @@
 export { default as RoomsList } from '$lib/components/RoomsList.svelte';
+export { default as JoinRoomModal } from '$lib/components/JoinRoomModal.svelte';
 export { default as MessagesArea } from '$lib/components/MessagesArea.svelte';
 export { default as MessageInput } from '$lib/components/MessageInput.svelte';
-export { default as JoinRoomModal } from '$lib/components/JoinRoomModal.svelte';
+
+export { default as CloseSign } from '$lib/components/svgs/CloseSign.svelte';
+export { default as SearchIcon } from '$lib/components/svgs/SearchIcon.svelte';
+export { default as TickedSign } from '$lib/components/svgs/TickedSign.svelte';

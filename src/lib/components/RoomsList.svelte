fac2a48ccda51ca5121246fcc2cfe35c2d6c5255
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-05-18 15:35:59 +0200

    feat: Added a button to join a new room

--- src/lib/components/RoomsList.svelte
+++ src/lib/components/RoomsList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { StyledText } from '@totocorpsoftwareinc/frontend-toolkit';
+	import { StyledButton, StyledText } from '@totocorpsoftwareinc/frontend-toolkit';
 	import type { RoomUiProps } from '$lib/communication/ui/roomUiProps';
 	import type { RoomUiDto } from '$lib/communication/ui/roomUiDto';
 
@@ -29,36 +29,43 @@
 	});
 </script>
 
-<div class="p-2">
-	<StyledText text="Rooms" styling="text-secondary text-sm font-semibold mb-2" />
-	<ul>
-		{#each rooms as room, id (room.id)}
-			<li class="mb-1">
-				{#if room.id === currentRoom}
-					<a
-						class="bg-primary-hover text-secondary flex w-full items-center justify-between rounded p-2 text-left"
-						href={'/chats/rooms/' + room.id}
-					>
-						{room.name}
-					</a>
-				{:else}
-					<a
-						class="hover:bg-primary-hover text-secondary flex w-full items-center justify-between rounded p-2 text-left"
-						href={'/chats/rooms/' + room.id}
-					>
-						{#if unread[id].unreadMessages == 0}
-							<span>{room.name}</span>
-						{:else}
-							<span class="font-bold">{room.name}</span>
-							<span
-								class="bg-secondary ml-1 rounded-full px-1.5 py-0.5 text-xs font-medium text-white"
-							>
-								{unread[id].unreadMessages}
-							</span>
-						{/if}
-					</a>
-				{/if}
-			</li>
-		{/each}
-	</ul>
+<div class="flex flex-grow flex-col justify-between p-2">
+	<div>
+		<StyledText text="Rooms" styling="text-secondary text-sm font-semibold mb-2" />
+		<ul>
+			{#each rooms as room, id (room.id)}
+				<li class="mb-1">
+					{#if room.id === currentRoom}
+						<a
+							class="bg-primary-hover text-secondary flex w-full items-center justify-between rounded p-2 text-left"
+							href={'/chats/rooms/' + room.id}
+						>
+							{room.name}
+						</a>
+					{:else}
+						<a
+							class="hover:bg-primary-hover text-secondary flex w-full items-center justify-between rounded p-2 text-left"
+							href={'/chats/rooms/' + room.id}
+						>
+							{#if unread[id].unreadMessages == 0}
+								<span>{room.name}</span>
+							{:else}
+								<span class="font-bold">{room.name}</span>
+								<span
+									class="bg-secondary ml-1 rounded-full px-1.5 py-0.5 text-xs font-medium text-white"
+								>
+									{unread[id].unreadMessages}
+								</span>
+							{/if}
+						</a>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<button
+		class="bg-secondary hover:bg-secondary-hover rounded-md px-4 py-2 text-sm font-medium text-white"
+		>Join a new room...</button
+	>
 </div>

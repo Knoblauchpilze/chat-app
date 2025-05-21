3f7abc6b5b0d51f17b1434054bd7e03da65cc1b9
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-21 07:57:48 +0200

    feat: Extracetd room entry into a dedicated component and added cross to leave a room

---
+++ src/lib/components/RoomEntry.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { RoomUiProps } from '$lib/communication/ui/roomUiProps';
+	import type { RoomUiDto } from '$lib/communication/ui/roomUiDto';
+	import { CloseSign } from '$lib/components';
+
+	interface Props {
+		selected: boolean;
+		roomProps: RoomUiProps | undefined;
+		room: RoomUiDto;
+	}
+
+	let { selected, roomProps, room }: Props = $props();
+
+	const roomStyle = 'text-secondary flex w-full items-center justify-between rounded p-2 text-left';
+	const backgroundStyle = $derived(selected ? 'bg-primary-hover' : 'hover:bg-primary-hover');
+</script>
+
+<div class="flex items-center">
+	<a class="{roomStyle} {backgroundStyle}" href={'/chats/rooms/' + room.id}>
+		{#if !roomProps || roomProps.unreadMessages == 0}
+			<span>{room.name}</span>
+		{:else}
+			<span class="font-bold">{room.name}</span>
+			<span class="bg-secondary ml-1 rounded-full px-1.5 py-0.5 text-xs font-medium text-white">
+				{roomProps.unreadMessages}
+			</span>
+		{/if}
+	</a>
+	<form method="POST" action="?/leaveRoom" class="absolute right-2 hidden group-hover:block">
+		<input type="hidden" name="roomId" value={room.id} />
+		<button
+			type="submit"
+			class="text-secondary-hover p-1 hover:text-red-500"
+			aria-label="Leave room"
+		>
+			<CloseSign />
+		</button>
+	</form>
+</div>
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,7 +1,8 @@
-export { default as RoomsList } from '$lib/components/RoomsList.svelte';
 export { default as JoinRoomModal } from '$lib/components/JoinRoomModal.svelte';
 export { default as MessagesArea } from '$lib/components/MessagesArea.svelte';
 export { default as MessageInput } from '$lib/components/MessageInput.svelte';
+export { default as RoomEntry } from '$lib/components/RoomEntry.svelte';
+export { default as RoomsList } from '$lib/components/RoomsList.svelte';
 
 export { default as CloseSign } from '$lib/components/svgs/CloseSign.svelte';
 export { default as SearchIcon } from '$lib/components/svgs/SearchIcon.svelte';
